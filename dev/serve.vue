71a35af7253197f112b5bf9a4f28c9d56b2b4294
Segers Benny <segersbenny@hotmail.con>
2020-09-20 00:09:02 +0200

    sample library

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import { VuseSample } from '@/entry';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    VuseSample,
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vuse-sample />
+  </div>
+</template>
